3210fe7013319440b88cd398f60b6cdea252f579
Braxen @ Work <braxen@braxnet.org>
2016-05-11 12:54:43 +0200

    fixed scrolling "viewed posts"

--- Enhanced_Ticker_Autoupdate_threads.user.js
+++ Enhanced_Ticker_Autoupdate_threads.user.js
@@ -2,16 +2,14 @@
 // @name        Enhanced Ticker: Autoupdate threads
 // @namespace   MrBrax
 // @description	Updates threads automatically if the ticker is open. Requires "Enhanced Ticker" 3.32 or upwards.
-// @updateURL   https://raw.githubusercontent.com/MrBrax/EnhancedTicker/master/Enhanced_Ticker_Autoupdate_threads.user.js
-// @downloadURL https://raw.githubusercontent.com/MrBrax/EnhancedTicker/master/Enhanced_Ticker_Autoupdate_threads.user.js
+// @updateURL   https://github.com/MrBrax/EnhancedTicker/raw/master/Enhanced_Ticker_Autoupdate_threads.user.js
+// @downloadURL https://github.com/MrBrax/EnhancedTicker/raw/master/Enhanced_Ticker_Autoupdate_threads.user.js
 // @include     https://facepunch.com/showthread.php?*
 // @include     https://facepunch.com/subscription.php*
 // @include     https://facepunch.com/usercp.php*
-// @version     0.14
+// @version     0.15
 // @grant       GM_addStyle
 // ==/UserScript==
-
-console.log( document.querySelectorAll("#navbar-login a:last-child") );
 
 function timeSince(date) {
 
@@ -45,107 +43,129 @@ GM_addStyle(".au_bar { background: #cce; border: 1px solid #777; border-bottom-w
 
 var thread = location.href.match(/\?t=([0-9]+)/);
 
-var paginator = document.getElementById("yui-gen1");
-var plist = document.getElementById("posts");
+if( thread ){
 
-if( paginator ){
-	var s = paginator.innerHTML.trim().match(/Page ([0-9]+) of ([0-9]+)/);
-	if(s[1] != s[2]){
-		var au_info = document.createElement("div");
-		au_info.className = "au_bar";
-		au_info.innerHTML = "<strong>Auto updater not running, go to the last page.</strong>";
-		plist.appendChild(au_info);
-		return;
+	var paginator = document.getElementById("yui-gen1");
+	var plist = document.getElementById("posts");
+
+	if( paginator ){
+		var s = paginator.innerHTML.trim().match(/Page ([0-9]+) of ([0-9]+)/);
+		if(s[1] != s[2]){
+			var au_info = document.createElement("div");
+			au_info.className = "au_bar";
+			au_info.innerHTML = "<strong>Auto updater not running, go to the last page.</strong>";
+			plist.appendChild(au_info);
+			return;
+		}
 	}
-}
 
-var title = document.title;
-var newposts = 0;
+	var title = document.title;
+	var newposts = 0;
 
-var unseenPosts = {};
+	var unseenPosts = {};
 
-document.title = "[" + newposts + "] " + title;
+	document.title = "[" + newposts + "] " + title;
 
-var au_info = document.createElement("div");
-au_info.className = "au_bar";
-au_info.innerHTML = "<strong>Auto updater starts here</strong>";
-plist.appendChild(au_info);
+	var au_info = document.createElement("div");
+	au_info.className = "au_bar";
+	au_info.innerHTML = "<strong>New posts will appear below if ticker is open</strong>";
+	plist.appendChild(au_info);
 
-function updateTime(a){
-	var t = document.querySelectorAll(".date");
-	for(i in t){
-		var d = new Date( t[i].title );
-		t[i].innerHTML = timeSince( d.getTime() ) + " Ago";
+	function updateTime(a){
+		var t = document.querySelectorAll(".date");
+		for(i in t){
+			var d = new Date( t[i].title );
+			t[i].innerHTML = timeSince( d.getTime() ) + " Ago";
+		}
+		if(!a) setTimeout( updateTime, 10000 );
 	}
-	if(!a) setTimeout( updateTime, 10000 );
-}
 
-updateTime();
+	updateTime();
 
-var storageHandler = function (e) {
-	if(e.key == "ETicker_LastPost"){
-		var d = e.newValue.split(".");
-		if(d[0] == thread[1]){
+	var storageHandler = function (e) {
 
-			if( document.getElementById("post_" + d[1] ) ){
-				console.log("Post already exists: " + d[1] );
-				return;
-			}
+		var is_new = e.key == "ETicker_LastPost";
+		var is_updated = e.key == "ETicker_UpdatePost"
 
-			var url = "https://facepunch.com/showthread.php?t=" + d[0] + "&p=" + d[1];
+		if( is_new || is_updated ){
+			var d = e.newValue.split(".");
+			if(d[0] == thread[1]){
 
-			console.log("Update thread", thread[1], url);
+				var post_exist = document.getElementById("post_" + d[1] );
 
-			var xhr = new XMLHttpRequest();
-			xhr.open("GET", url, true);
-			xhr.responseType = "document";
-			xhr.onreadystatechange = function (){
-				if (xhr.readyState == 4 && xhr.status == 200){
-
-					var data = xhr.responseXML;
-					var newp = data.getElementById("post_" + d[1]);
-
-					if(newp){
-						// new page notifier
-						if(newp.parentNode.childNodes[1] == newp){
-							var s = document.createElement("div");
-							s.className = "au_bar";
-							s.innerHTML = "<strong>New page</strong>";
-							plist.appendChild(s);
-							console.log("new page");
-						}
-						// add new post
-						plist.appendChild(newp);
-					}
-
-					// update title
-					unseenPosts[ d[1] ] = true;
-					document.title = "[" + ( Object.keys(unseenPosts).length ) + "] " + title;
-
-					// update ratings
-					var ratings = data.querySelectorAll(".rating_results");
-					for(i in ratings){
-						document.getElementById( ratings[i].id ).innerHTML = ratings[i].innerHTML;
-					}
-
-					updateTime(true);
+				if( post_exist && e.key == "ETicker_LastPost" ){
+					console.log("Post already exists: " + d[1] );
+					return;
 				}
-			};
-			xhr.send();
-		}
-	}
-};
 
-var scrollHandler = function (e) {
-	for(i in unseenPosts){
-		var element = document.getElementById("post_" + i);
-		if(!element){ console.log("no element for post " + i); continue; }
-  		if( window.pageYOffset > element.getBoundingClientRect().top ) {
-			delete unseenPosts[ d[1] ];
-			document.title = "[" + ( Object.keys(unseenPosts).length ) + "] " + title;
+				var url = "https://facepunch.com/showthread.php?t=" + d[0] + "&p=" + d[1];
+
+				console.log("Update thread", thread[1], url);
+
+				var xhr = new XMLHttpRequest();
+				xhr.open("GET", url, true);
+				xhr.responseType = "document";
+				xhr.onreadystatechange = function (){
+					if (xhr.readyState == 4 && xhr.status == 200){
+
+						var data = xhr.responseXML;
+						var newp = data.getElementById("post_" + d[1]);
+
+						if(newp){
+							// new page notifier
+							if(newp.parentNode.childNodes[1] == newp){
+								var page = data.getElementById("yui-gen1").match(/Page ([0-9]+)/)[1];
+								var s = document.createElement("div");
+								s.className = "au_bar";
+								s.innerHTML = "<strong><a href='https://facepunch.com/showthread.php?t=" + d[0] + "&page=" + page + "'>Page " + page + "</a></strong>";
+								plist.appendChild(s);
+								console.log("new page");
+							}
+
+							// add new post
+							if( is_new ){
+								newp.className = newp.className.replace("postbitold", "postbitnew");
+								plist.appendChild(newp);
+							}else{
+								post_exist.innerHTML = newp.innerHTML;
+							}
+						}
+
+						// update title
+						if( is_new ){
+							unseenPosts[ d[1] ] = true;
+							document.title = "[" + ( Object.keys(unseenPosts).length ) + "] " + title;
+						}
+
+						// update ratings
+						var ratings = data.querySelectorAll(".rating_results");
+						for(i in ratings){
+							document.getElementById( ratings[i].id ).innerHTML = ratings[i].innerHTML;
+						}
+
+						updateTime(true);
+					}
+				};
+				xhr.send();
+			}
+		}
+	};
+
+	var scrollHandler = function (e) {
+		for(i in unseenPosts){
+			var element = document.getElementById("post_" + i);
+			if( !element ){ console.log("no element for post " + i); continue; }
+			//console.log( i, element.getBoundingClientRect().top );
+	  		if( element.getBoundingClientRect().top < 0 ) {
+	  			console.log("Post now seen: " + i);
+				delete unseenPosts[ i ];
+				element.className = element.className.replace("postbitnew", "postbitold");
+				document.title = "[" + ( Object.keys(unseenPosts).length ) + "] " + title;
+			}
 		}
 	}
+
+	window.addEventListener("storage", storageHandler, false);
+	window.addEventListener('scroll', scrollHandler, false);
+
 }
-
-window.addEventListener("storage", storageHandler, false);
-window.addEventListener('scroll', scrollHandler, false);
